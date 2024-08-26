56e76fc71f96a5fbbc0d701c23ad401eea5555c9
jager03 <johannesjt@yahoo.es>
2024-08-26 13:58:48 +0200

    refactor: added throwing IOException to the Reader interface and deleted redundant "public modifier"

--- src/main/java/de/exxcellent/challenge/interfaces/Reader.java
+++ src/main/java/de/exxcellent/challenge/interfaces/Reader.java
@@ -2,8 +2,10 @@ package de.exxcellent.challenge.interfaces;
 
 import de.exxcellent.challenge.classes.FootballTeam;
 import de.exxcellent.challenge.classes.Weather;
+
+import java.io.IOException;
 import java.util.List;
-public interface Reader {
-    public List<Weather> readWeatherData(String filePath);
-    public List<FootballTeam> readFootballData(String filePath);
+interface Reader {
+    public List<Weather> readWeatherData(String filePath) throws IOException;
+    public List<FootballTeam> readFootballData(String filePath) throws IOException;
 }
